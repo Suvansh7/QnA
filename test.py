bb093e70ed5e9e2ed11aa314397f4ae1d244d953
Suvansh7 <96539773+Suvansh7@users.noreply.github.com>
2024-08-13 19:22:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 corpus = ""
 def next_question(ans,corpus) :
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True,google_api_key="AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E")
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True,google_api_key="API-KEY")
     ans = model(
     [
     SystemMessage(content="Only Generate a single line next response relevant to given answer to evaluate the candidate skills, As you are taking an interview"),
